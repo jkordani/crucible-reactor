632f2ad40e7ad0c371c45da5ea915e64fa6306a3
Joshua Kordani <jkordani@roboticresearch.com>
2021-12-07 17:58:50 -0500

    This took entirely too long to make work

--- crucible-reactor.lisp
+++ crucible-reactor.lisp
@@ -1,3 +1,37 @@
 ;;;; crucible-reactor.lisp
 
 (in-package #:crucible-reactor)
+
+(defparameter *auth-token* nil)
+
+(defparameter *base-url* nil)
+
+(defun login (username password base-url)
+  (setf *base-url* base-url)
+  (setf *auth-token*
+        (request-endpoint "/auth/login" :method :post :params `(("userName" . ,username)
+                                                                ("password" . ,password)))))
+
+(defun request-endpoint (endpoint &key (method :get) (params nil))
+  (if (null *base-url*)
+      (error "Not Logged in")
+      (multiple-value-bind (response return-code headers)
+          (drakma:http-request
+           (format nil "~a/~a" *base-url* endpoint)
+           :method method
+           :force-binary t
+           :content-type "application/json"
+           :parameters (append params
+                               (if (not (null *auth-token*))
+                                   (list (list* "FEAUTH" (rest (first *auth-token*))))
+                                   nil)))
+
+        (if (/= return-code 200)
+            (error "Request failed ~a" return-code))
+
+        (values (json:decode-json-from-string
+                 (flexi-streams:octets-to-string response))
+                return-code (cdr (assoc :content-type headers))))))
+
+(defun get-users ()
+  (request-endpoint "/admin/users"))
