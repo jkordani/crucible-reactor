cc40c039ef922451e5ed73c265c0b4baf1cce8c4
Joshua Kordani <jkordani@roboticresearch.com>
2021-12-07 19:27:08 -0500

    Filtered out spurious / in urls

--- crucible-reactor.lisp
+++ crucible-reactor.lisp
@@ -9,7 +9,7 @@
 (defun login (username password base-url)
   (setf *base-url* base-url)
   (setf *auth-token*
-        (request-endpoint "rest-service-fecru/auth/login" :method :post
+        (request-endpoint "/rest-service-fecru/auth/login" :method :post
                                                            :params `(("userName" . ,username)
                                                                      ("password" . ,password)))))
 
@@ -18,7 +18,9 @@
       (error "Not Logged in")
       (multiple-value-bind (response return-code headers)
           (drakma:http-request
-           (format nil "~a/~a" *base-url* endpoint)
+           (format nil "~a/~a"
+                   (string-right-trim '(#\/) *base-url*)
+                   (string-left-trim '(#\/) endpoint))
            :method method
            :force-binary t
            :content-type "application/json"
@@ -35,4 +37,4 @@
                 return-code (cdr (assoc :content-type headers))))))
 
 (defun get-users ()
-  (request-endpoint "rest-service-fecru/admin/users"))
+  (request-endpoint "/rest-service-fecru/admin/users/"))
